b93bd1ffa60eb45d781a39b0356d2ffd2b07b470
Fábio Souza <fsouza.me@gmail.com>
2020-08-31 15:37:24 -0300

    Adds image card component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
 
-  <div>
-    <h1>{{ title }}</h1>
+  <div class="container">
+    <h1 class="page-title">{{ title }}</h1>
 
-    <ul>
-      <li :key="image.url" v-for="image of images">
-        <img :src="image.url" :alt="image.titulo">
+    <input
+      type="search"
+      name="filter"
+      class="btn-filter"
+      @input="filter = $event.target.value"
+      placeholder="Pesquise pelo nome da imagen"
+    />
+
+    <ul class="image-list">
+      <li class="image-list-item" :key="image._id" v-for="image of filterImages">
+        <image-card-component :title="image.titulo">
+          <img class="image-responsive" :src="image.url" :alt="image.titulo" />
+        </image-card-component>
       </li>
     </ul>
   </div>
@@ -13,12 +23,31 @@
 </template>
 
 <script>
+import Card from './components/shared/image-card/Card.vue';
+
 export default {
+  components: {
+    'image-card-component': Card,
+  },
+
   data() {
     return {
       title: 'Vuepic',
       images: [],
+      filter: '',
     };
+  },
+
+  computed: {
+    filterImages() {
+      if (this.filter) {
+        let exp = new RegExp(this.filter.trim(), 'i');
+
+        return this.images.filter(image => exp.test(image.titulo));
+      }
+
+      return this.images;
+    },
   },
 
   created() {
@@ -31,5 +60,31 @@ export default {
 </script>
 
 <style lang="scss">
+.container {
+  font-family: Arial, Helvetica, sans-serif;
+  width: 96%;
+  margin: 0 auto;
+}
 
+.page-title {
+  text-align: center;
+}
+
+.image-list {
+  list-style: none;
+}
+
+.image-list .image-list-item {
+  display: inline-block;
+}
+
+.btn-filter {
+  display: block;
+  width: 100%;
+  height: 30px;
+}
+
+.image-responsive {
+  width: 100%;
+}
 </style>
---
+++ src/components/shared/image-card/Card.vue
@@ -0,0 +1,68 @@
+<template>
+
+  <div class="image-card">
+    <h2 class="image-card-title" @dblclick="visibled = !visibled">
+      {{ title }}
+    </h2>
+
+    <transition name="fade-card">
+      <div class="image-card-content" v-show="visibled">
+        <slot></slot>
+      </div>
+    </transition>
+  </div>
+
+</template>
+
+<script>
+export default {
+  props: ['title'],
+
+  data() {
+    return {
+      visibled: true,
+    };
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.image-card {
+  padding: 0 auto;
+  border: solid 2px grey;
+  display: inline-block;
+  margin: 5px;
+  box-shadow: 5px 5px 10px grey;
+  width: 200px;
+  height: 100%;
+  vertical-align: top;
+  text-align: center;
+}
+
+.image-card .image-card-title {
+  text-align: center;
+  border: solid 2px;
+  background: lightblue;
+  margin: 0 0 15px 0;
+  padding: 10px;
+  text-transform: uppercase;
+}
+
+.image-card-title {
+  cursor: pointer;
+}
+
+* {
+  box-shadow: 5px 5px 5px;
+}
+
+.fade-card-enter,
+.fade-card-leave-active {
+  opacity: 0;
+}
+
+.fade-card-enter-active,
+.fade-card-leave-active {
+  transition: opacity .4s;
+}
+</style>

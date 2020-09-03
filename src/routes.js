32e1f0e015a538d06bda08df5f97928bc985572c
Fábio Souza <fsouza.me@gmail.com>
2020-09-03 17:47:08 -0300

    Adds lazy loading in routes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import Home from './components/home/Home.vue';
-import Register from './components/register/Register.vue';
-import NotFound from './components/not-found/NotFound.vue';
+const Register = () => System.import('./components/register/Register.vue');
+const NotFound = () => System.import('./components/not-found/NotFound.vue');
 
 export const routes = [
   { path: '', name: 'home', component: Home, label: 'Home', show: true, },
